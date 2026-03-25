555c508b11b4d84e86e89a28f79341d8234ffdda
Lorenzo <lorenzocacciotti514@gmail.com>
2026-03-25 15:14:48 +0100

    Realizzato UserService

---
+++ src/app/user-service.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { OperationTypeByVendor, PortalUser } from './model/entities';
+import { map, Observable, of, tap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+    private http = inject(HttpClient);
+    private apiUrl = 'https://localhost:8080/api/users';
+    private _allUsers: PortalUser[] | null = null;
+
+    public getAllUsers(): Observable<PortalUser[]> {
+        if (this._allUsers) {
+          return of(this._allUsers);
+        }
+        return this.http.get<PortalUser[]>(this.apiUrl).pipe(
+          tap(users => this._allUsers = users)
+        );
+    }
+
+    public findByOperationType(operationTypeId: number): Observable<PortalUser[]> {
+      return this.getAllUsers().pipe(
+        map(users => users.filter(user => user.operationProvided?.some(op => op.operationType.id === operationTypeId)))
+      )
+    }
+  
+}
