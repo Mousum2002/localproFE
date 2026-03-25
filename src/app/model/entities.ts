0d480fdb39d2c9c3c6179ab5b417acc7e28c9ad7
EliasRaccoon <angelorusu24@gmail.com>
2026-03-25 11:19:25 +0100

    Creato entità

---
+++ src/app/model/entities.ts
@@ -0,0 +1,48 @@
+export interface PortalUser 
+{
+    id?: number;
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
+    address: string;
+    city: string;
+    bio: string
+    x: number;
+    y: number;
+    reviews: Review[];
+    operationProvided: OperationTypeByVendor[];
+}
+
+export interface Review
+{
+    id?: number;
+    rating: number;
+    description: string;
+}
+
+export enum Status {
+    OPEN = 'OPEN',
+    CLOSED = 'CLOSED',
+    PENDING = 'PENDING'
+}
+
+export interface OperationType
+{
+    id?: number;
+    userId: number;
+    name: string;
+    tags: string[];
+    description: string;
+    status: Status;
+    operationProvided: OperationTypeByVendor[];
+
+}
+
+export interface OperationTypeByVendor
+{
+    id?: number;
+    portalUser: PortalUser;
+    operationType: OperationType;
+    price: number;
+}
