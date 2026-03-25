a2d4b49aee66846834a4d406ce9a7bf742114cd4
EliasRaccoon <angelorusu24@gmail.com>
2026-03-25 11:52:54 +0100

    Sistemato ProCard e ProList
    in ProCard tolto portalUser = = input.required<PortalUser>(); e messo
    @Input() portalUser: any;
    
    invece nel ProListHtml cambiato portalUser in user per sovrapposizione dei nomi nel template.

--- src/app/pro-card/pro-card.ts
+++ src/app/pro-card/pro-card.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-pro-card',
+  standalone: true,
   imports: [],
   templateUrl: './pro-card.html',
   styleUrl: './pro-card.css',
 })
 export class ProCard 
 {
-  portalUser = = input.required<PortalUser>();
+  @Input() portalUser: any; // Replace 'any' with your actual User interface/type
 }
