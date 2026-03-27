ea0a68638a70387e7d65ca7defceb9cdb12eb3f5
portanovadomenico-dot <portanova.domenico@gmail.com>
2026-03-27 17:51:31 +0100

    Removed unnecessary timeout on registration.

--- src/app/register-page/register-page.ts
+++ src/app/register-page/register-page.ts
@@ -63,7 +63,7 @@ export class RegisterPage {
       next: () => {
         this.loading.set(false);
         this.success.set(true);
-        setTimeout(() => this.router.navigate(['/home']), 2000);
+        this.router.navigate(['/home']);
       },
       error: () => {
         this.loading.set(false);
