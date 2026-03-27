345a4d84bc8b9e3198c4910a787c5deaefa6449d
portanovadomenico-dot <portanova.domenico@gmail.com>
2026-03-27 16:57:24 +0100

    Created admin page component

---
+++ src/app/admin-page/admin-page.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-page',
+  imports: [],
+  templateUrl: './admin-page.html',
+  styleUrl: './admin-page.css',
+})
+export class AdminPage 
+{
+
+  
+
+}
